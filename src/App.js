4d1b019eb74efcdf2241552866f0bde94b20a33c
Inglish Foust <99756614+inglish2022@users.noreply.github.com>
2022-08-13 19:38:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import About from './components/About';
+import Nav from './components/Nav';
+// import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
-function App() {
+// import { useEffect } from "react";
+// import logo from './aaliyah.JPG';
+// import './App.css';
+
+// function App() {
+  function App() {
+    // You can skip useEffect if you're not using TailwindCSS
+    // Otherwise, for the production usage refer to https://tailwindcss.com/docs/installation
+    useEffect(() => {
+      if (document) {
+        const stylesheet = document.createElement("link");
+        stylesheet.rel = "stylesheet";
+        stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+  
+        document.head.appendChild(stylesheet);
+      }
+    }, []);
+  const [categories] = useState([
+    
+           
+        { name: "babies", description: "Photos of newborns and babies",},
+        { name: "couples", description: "Engagements and couples" },
+        { name: "seniors", description: "Graduates and singles"},
+    ]);
+    const [currentCategory, setCurrentCategory] = useState(categories[0]);
+    const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav 
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+    ></Nav>
+      <main>
+        {!contactSelected ? (
+          <>
+          <About></About>
+          {/* <Gallery currentCategory={currentCategory}></Gallery> */}
+          
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+ 
+      </main>
     </div>
   );
 }
 
 export default App;
+
